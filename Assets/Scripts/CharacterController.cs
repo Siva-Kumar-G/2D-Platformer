dee68900246881362c1a7bc5898c4457da6b1342
Siva_Kumar_G <sivakumar9790020610@gmail.com>
2020-12-01 14:29:09 +0530

    fixed  a bug when forward,backward, jumb button  clicked the buttons are disable

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    private void Start()
+    {
+
+    }
+    private void Update()
+    {
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(new Vector2(10, 0));
+        }
+
+
+
+
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(new Vector2(-10, 0));
+        }
+    }
+}
