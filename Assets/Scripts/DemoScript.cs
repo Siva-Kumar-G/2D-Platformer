ca11d17931ec0b0d09fabc04dd2723aa18a8e9cf
sai-sham <73777552+sai-sham@users.noreply.github.com>
2020-12-18 11:49:11 +0530

    lvl2
    fixed the gg properly

---
+++ Assets/Scripts/DemoScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoScript : MonoBehaviour
+{
+    public void DoGamePause()
+    {
+        Time.timeScale = 0;
+    }
+}
---
+++ Assets/Scripts/sham.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sham : MonoBehaviour
+{
+ public void quitapp()
+    {
+        Application.Quit();
+    }
+}
